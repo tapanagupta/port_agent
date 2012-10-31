f6d72145f7b7c3012b5c02fc40672ab33cbfbe47
Bill French <wfrench@ucsd.edu>
2012-10-31 13:18:24 -0700

    added version header for binary version constant definition

---
+++ src/version.h
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * Filename: version.h
+ * Author: Bill French (wfrench@ucsd.edu)
+ * License: Apache 2.0
+ *
+ * Version number for port agent binaries and shared libraries
+ ******************************************************************************/
+
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define PORT_AGENT_VERSION "0.1.0"
+
+#endif // VERSION_H
