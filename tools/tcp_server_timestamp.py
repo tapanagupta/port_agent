4d59c22b6007d7f6fb189ecbda19001e64d067a0
kstiemke <stiemke27@gmail.com>
2014-06-23 08:20:39 -0700

    add file back

---
+++ tools/tcp_server_timestamp.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#
+# Utility that accepts TCP socket connections and spits out a 
+# random stream of data.
+#
+
+import socket 
+import errno 
+import sys 
+import time
+ 
+##let's set up some constants
+HOST = ''    #we are the host
+PORT = 4001    #arbitrary port not currently in use
+ADDR = (HOST,PORT)    #we need a tuple for the address
+BUFSIZE = 4096    #reasonably sized buffer for data
+TIMEOUT = 30         # Socket timeout
+
+## now we create a new socket object (serv)
+## see the python docs for more information on the socket types/flags
+serv = socket.socket( socket.AF_INET,socket.SOCK_STREAM)    
+ 
+##bind our socket to the address
+serv.bind((ADDR))    #the double parens are to create a tuple with one element
+serv.listen(5)
+serv.settimeout(TIMEOUT)
+
+conn,addr = serv.accept() #accept the connection
+
+
+while True:
+	try:
+		conn.send("%19.9f\n" % time.time() );
+		time.sleep(3);
+	except socket.error as e:
+		# [Errno 35] Resource temporarily unavailable.
+		if e.errno == errno.EAGAIN:
+			pass
+		else:
+			raise e
+
+
+try:
+	conn.close()
+except Exception, e:
+	raise e
